ec9f4270350046655dd5709a67e9c7800f85f1e1
amangeldimasakovwork1 <amangeldimasakov.work1@gmail.com>
2026-02-18 19:47:30 +0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//main.ts
+//main.ts MugtVpnsBot
 import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
 const kv = await Deno.openKv();
 const TOKEN = Deno.env.get("BOT_TOKEN");
@@ -879,4 +879,5 @@ serve(async (req: Request) => {
   }
   return new Response("OK", { status: 200 });
 });
+
 
